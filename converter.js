75f67253aa6960ff0709ffcfa6022de26cb6611d
eswaranthangavel23 <eswaran.thangavel@changecx.com>
2022-04-20 22:52:55 +0530

    initial commit

---
+++ converter.js
@@ -0,0 +1,235 @@
+var fs = require("fs");
+var obj;
+
+const convertor = async () => {
+  let attr = [
+    "arm",
+    "armColor",
+    "armpad",
+    "arms",
+    "back",
+    "backSupport",
+    "backSuspension",
+    "base",
+    "basecolorbasefinishbaseFrame",
+    "basetype",
+    "cableTrough",
+    "caster",
+    "casterglide",
+    "color",
+    "colorconfiguration",
+    "cordCable",
+    "cordlength",
+    "cutout",
+    "deskTopFinish",
+    "detail",
+    "fabric",
+    "fileDrawer",
+    "finish",
+    "frame",
+    "framebase",
+    "frameBaseAndSuspension",
+    "frameColor",
+    "frameCord",
+    "frameFinish",
+    "grommets",
+    "handle",
+    "hardware",
+    "headboard",
+    "height",
+    "heightAdjustment",
+    "heightRange",
+    "kitType",
+    "leg",
+    "legColor",
+    "legFinish",
+    "legs",
+    "legstyle",
+    "material",
+    "orientation",
+    "panels",
+    "powerAccess",
+    "powerAdjustment",
+    "pulls",
+    "seat",
+    "seatDepth",
+    "seatFabric",
+    "seatmaterial",
+    "seatStyle",
+    "shape",
+    "shell",
+    "shellMaterial",
+    "size",
+    "softfirm",
+    "storage",
+    "style",
+    "tilt",
+    "top",
+    "topFinish",
+    "upholstery",
+    "woodfinish",
+  ];
+  fs.readFile("./csvjson.json", "utf8", function (err, data) {
+    if (err) throw err;
+    obj = JSON.parse(data);
+    let type = "";
+    let resultJson = [];
+    for (let o of obj) {
+      if (o.productType != "") type = o.productType;
+
+      let result = {
+        productType: o.productType,
+        variantId: o.variantId,
+        sku: o.sku,
+        prices: o.prices,
+        tax: o.tax,
+        categories: o.categories,
+        images: o.images,
+        "name.en": o["name.en"],
+        "description.en": o["description.en"],
+        "slug.en": o["slug.en"],
+        "metaDescription.en": o["metaDescription.en"],
+        "metaKeywords.en": o["metaKeywords.en"],
+
+        "name.de": o["name.de"],
+        "description.de": o["description.de"],
+        "slug.de": o["slug.de"],
+        "metaDescription.de": o["metaDescription.de"],
+        "metaKeywords.de": o["metaKeywords.de"],
+        "metaTitle.de": o["metaTitle.de"],
+
+        "name.it": o["name.it"],
+        "description.it": o["description.it"],
+        "slug.it": o["slug.it"],
+        "metaDescription.it": o["metaDescription.it"],
+        "metaKeywords.it": o["metaKeywords.it"],
+        "metaTitle.it": o["metaTitle.it"],
+        creationDate: "",
+        baseId: o.baseId,
+        "details.it": "",
+        isOnStock: "",
+        isLook: "",
+        lookProducts: "",
+        seasonNew: "",
+      };
+      for (let att of attr) {
+        result[type + "_" + att] = o[att];
+      }
+      resultJson.push(result);
+    }
+    let resultJsondata = JSON.stringify(resultJson);
+    fs.writeFileSync("resultJson.json", resultJsondata);
+  });
+};
+//convertor();
+
+const facts = () => {
+  let attr = [
+    "arm",
+    "armColor",
+    "armpad",
+    "arms",
+    "back",
+    "backSupport",
+    "backSuspension",
+    "base",
+    "basecolorbasefinishbaseFrame",
+    "basetype",
+    "cableTrough",
+    "caster",
+    "casterglide",
+    "color",
+    "colorconfiguration",
+    "cordCable",
+    "cordlength",
+    "cutout",
+    "deskTopFinish",
+    "detail",
+    "fabric",
+    "fileDrawer",
+    "finish",
+    "frame",
+    "framebase",
+    "frameBaseAndSuspension",
+    "frameColor",
+    "frameCord",
+    "frameFinish",
+    "grommets",
+    "handle",
+    "hardware",
+    "headboard",
+    "height",
+    "heightAdjustment",
+    "heightRange",
+    "kitType",
+    "leg",
+    "legColor",
+    "legFinish",
+    "legs",
+    "legstyle",
+    "material",
+    "orientation",
+    "panels",
+    "powerAccess",
+    "powerAdjustment",
+    "pulls",
+    "seat",
+    "seatDepth",
+    "seatFabric",
+    "seatmaterial",
+    "seatStyle",
+    "shape",
+    "shell",
+    "shellMaterial",
+    "size",
+    "softfirm",
+    "storage",
+    "style",
+    "tilt",
+    "top",
+    "topFinish",
+    "upholstery",
+    "woodfinish",
+  ];
+  let types = [
+    "table",
+    "chair",
+    "desk",
+    "storage",
+    "light",
+    "stool",
+    "sofa",
+    "bed",
+    "bundle",
+    "gaming",
+  ];
+
+  // {
+  //   facet: "variants.attributes.arm.key",
+  //   type: "string",
+  //   option: "",
+  //   name: "arm"
+  // },
+
+  let results = [];
+  let finalattr = [];
+  for (let type of types) {
+    for (let att of attr) {
+      let res = {
+        facet: "variants.attributes." + type + "_" + att + ".key",
+        type: "string",
+        option: "",
+        name: type + "_" + att,
+      };
+      results.push(res);
+      finalattr.push(type + "_" + att);
+    }
+  }
+  //console.log()
+  let resultJsondata = JSON.parse(results);
+  fs.writeFileSync("facetsJson.txt", results);
+
+  // let attrJsondata = JSON.stringify(finalattr);
+  // fs.writeFileSync("finalattr.json", attrJsondata);
+};
+facts();
---
+++ merge/merge.js
@@ -0,0 +1,135 @@
+var fs = require("fs");
+
+const convertor = async () => {
+  let resultJson = [];
+  let attributes = [];
+  let types = ["desk", "chair", "Table"];
+  fs.readFile("./ESWARAN.json", "utf8", function (err, data) {
+    if (err) throw err;
+    let eswaran = JSON.parse(data);
+    let count = 1;
+    for (let data of eswaran) {
+      let outData = {
+        productType: data["product-type"],
+        variantId: data["varient-id"],
+        sku: 100000 + count,
+        prices: data.price,
+        tax: "standard",
+        categories: "",
+        images: data.image,
+        "name.en": data.name,
+        "description.en": data.description,
+        "slug.en":
+          data.name
+            .replace(/[^a-zA-Z ]/g, "")
+            .replace(/\s/g, "-")
+            .toLowerCase()
+            .replace("--", "-") +
+          "-" +
+          (100000 + count),
+        "metaDescription.en": "",
+        "metaKeywords.en": "",
+        "name.de": data.name,
+        "description.de": data.description,
+        "slug.de":
+          data.name
+            .replace(/[^a-zA-Z ]/g, "")
+            .replace(/\s/g, "-")
+            .toLowerCase()
+            .replace("--", "-") +
+          "-" +
+          (100000 + count),
+        "metaDescription.de": "",
+        "metaKeywords.de": "",
+        "metaTitle.de": "",
+        "name.it": data.name,
+        "description.it": data.description,
+        "slug.it":
+          data.name
+            .replace(/[^a-zA-Z ]/g, "")
+            .replace(/\s/g, "-")
+            .toLowerCase()
+            .replace("--", "-") +
+          "-" +
+          (100000 + count),
+        "metaDescription.it": "",
+        "metaKeywords.it": "",
+        "metaTitle.it": "",
+        creationDate: "",
+        baseId: count,
+      };
+      for (const [key, value] of Object.entries(data)) {
+        if (types.includes(key.split("_")[0])) {
+          if (!attributes.includes(key)) attributes.push(key);
+          outData[key] = value;
+        }
+      }
+      resultJson.push(outData);
+      count++;
+    }
+    let resultJsondata = JSON.stringify(resultJson);
+    fs.writeFileSync("resultJson.json", resultJsondata);
+
+    let resultattr = JSON.stringify(attributes);
+    fs.writeFileSync("resultattr.json", resultattr);
+
+    getProducttypes(resultJson);
+  });
+};
+const getProducttypes = (resultJson) => {
+  let producttypes = [];
+
+  fs.readFile("./resultattr.json", "utf8", function (err, data) {
+    if (err) throw err;
+    let attrs = JSON.parse(data);
+    //console.log(resultJson.length, "length");
+    for (let attr of attrs) {
+      let attrValues = [];
+      let attr_values = [];
+      for (let product of resultJson) {
+        for (const [key, value] of Object.entries(product)) {
+          if (attr == key) {
+            if (value != "") {
+              if (!attr_values.includes(value)) {
+                attr_values.push(value);
+                attrValues.push({
+                  key: value,
+                  label: value,
+                });
+              }
+            }
+          }
+        }
+      }
+      let at = ["desk", "chair", "Table"];
+      let attrName = "";
+      for (let a of at) {
+        if (attr.split("_")[0] == a) attrName = attr.replace(a + "_", "");
+      }
+      let result = {
+        name: attr,
+        label: {
+          en: attrName,
+          it: attrName,
+          de: attrName,
+        },
+        isRequired: false,
+        type: {
+          name: "enum",
+          values: attrValues.sort((a, b) => (a.key > b.key ? 1 : -1)),
+        },
+        attributeConstraint: "None",
+        isSearchable: true,
+        inputHint: "SingleLine",
+        displayGroup: "Other",
+      };
+      producttypes.push(result);
+    }
+    let producttypesdata = JSON.stringify(producttypes);
+    fs.writeFileSync("producttypes.json", producttypesdata);
+
+    // let resultattr = JSON.stringify(attributes);
+    // fs.writeFileSync("resultattr.json", resultattr);
+  });
+};
+convertor();
